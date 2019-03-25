370899322e2553e81c2e78d868d56ca1df20194b
Aleksander Molak <aleksander.molak@gmail.com>
2019-03-25 10:35:04 +0100

    Update [IMPL]_Deep_Autoencoder_Keras.py

--- [IMPL]_Deep_Autoencoder_Keras.py
+++ [IMPL]_Deep_Autoencoder_Keras.py
@@ -1,3 +1,6 @@
+# This code is partially authored by the Authors of Coursera's Deep Learning Specialization by HSE & Yandex:
+# https://www.coursera.org/learn/intro-to-deep-learning/
+
 from keras import layers as L
 
 def build_deep_autoencoder(img_shape, code_size):
@@ -41,3 +44,24 @@ def build_deep_autoencoder(img_shape, code_size):
     decoder.add(L.Conv2DTranspose(3, kernel_size=(3,3), strides=2, activation=None, padding='same'))
     
     return encoder, decoder
+
+# Build and train autoencoder
+s = reset_tf_session()
+
+encoder, decoder = build_deep_autoencoder(IMG_SHAPE, code_size=32)
+
+inp = L.Input(IMG_SHAPE)
+code = encoder(inp)
+reconstruction = decoder(code)
+
+autoencoder = keras.models.Model(inputs=inp, outputs=reconstruction)
+autoencoder.compile(optimizer="adamax", loss='mse')
+
+autoencoder.fit(x=X_train, y=X_train, epochs=25,
+                validation_data=[X_test, X_test],
+                callbacks=[keras_utils.ModelSaveCallback(model_filename),
+                           keras_utils.TqdmProgressCallback()],
+                verbose=0,
+                initial_epoch=last_finished_epoch or 0)
+
+
